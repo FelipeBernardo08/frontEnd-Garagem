f3cd23b826e2c84cf9f0baaba6f82852d07f7032
Felipe Bernardo <felipebernardo55@hotmail.com>
2024-04-16 23:36:33 -0300

    finalizacao componente criar despesas

--- src/app/componentes/despesas/criar-despesas/criar-despesas.component.ts
+++ src/app/componentes/despesas/criar-despesas/criar-despesas.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Despesas } from 'src/app/interface/despesas';
+import { CarroService } from 'src/app/services/carro.service';
 import { ClienteService } from 'src/app/services/cliente.service';
+import { DespesasService } from 'src/app/services/despesas.service';
+import { MotoService } from 'src/app/services/moto.service';
 import { UserService } from 'src/app/services/user.service';
 import { VendaService } from 'src/app/services/venda.service';
 
@@ -14,7 +18,11 @@ export class CriarDespesasComponent implements OnInit {
   constructor(
     private clienteService: ClienteService,
     private userService: UserService,
-    private vendaService: VendaService
+    private vendaService: VendaService,
+    private carroService: CarroService,
+    private motoService: MotoService,
+    private despesaService: DespesasService,
+    private router: Router
   ) { }
 
   despesas: Despesas = {
@@ -30,8 +38,21 @@ export class CriarDespesasComponent implements OnInit {
   clientes: Array<any> = [];
   vendedores: Array<any> = [];
   vendas: Array<any> = [];
+  carros: Array<any> = [];
+  motos: Array<any> = [];
+
+  selectVeiculo: string = '';
 
   ngOnInit(): void {
+    const forms = document.querySelectorAll('.needs-validation')
+    Array.from(forms).forEach((form: any) => {
+      form.addEventListener('submit', (event: any) => {
+        if (form.classList == 'ng-valid') {
+        }
+        form.classList.add('was-validated')
+      }, false)
+    })
+
     this.clienteService.readClientes().subscribe((cliente: any) => {
       this.clientes = cliente
     });
@@ -43,6 +64,39 @@ export class CriarDespesasComponent implements OnInit {
     this.vendaService.getVenda().subscribe((venda: any) => {
       this.vendas = venda
     })
+  }
+
+  selecionarVeiculo(e: any): void {
+    let event = e.target.value
+    if (event == 'carro') {
+      this.buscarCarro();
+    } else {
+      this.buscarMoto();
+    }
+  }
+
+  buscarCarro(): void {
+    this.selectVeiculo = 'carro'
+    this.carroService.readCarro().subscribe((carro: any) => {
+      this.carros = carro
+    });
+  }
+
+  buscarMoto(): void {
+    this.selectVeiculo = 'moto'
+    this.motoService.readMoto().subscribe((moto: any) => {
+      this.motos = moto
+    });
+  }
+
+  enviar(): void {
+    let form = document.getElementById('formDespesas')
+    let valid = form?.classList.contains('ng-valid')
+    if (valid) {
+      this.despesaService.createDespesas(this.despesas).subscribe(() => {
+        this.router.navigate(['/despesas'])
+      })
+    }
   }
 
 }
---
+++ src/app/services/despesas.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Despesas } from '../interface/despesas';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DespesasService {
+
+  constructor(private http: HttpClient) { }
+
+  baseUrl = 'http://127.0.0.1:8000/api';
+
+  createDespesas(despesas: any): Observable<Despesas> {
+    return this.http.post<Despesas>(`${this.baseUrl}/despesas`, despesas);
+  }
+
+}
